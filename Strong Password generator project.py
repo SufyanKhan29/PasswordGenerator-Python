41878379a3a493f7761f5464b3f56ea2c0ce709b
sufyankhan29 <assifankhan15766@gmail.com>
2025-03-23 14:37:50 +0500

    Password Generator

--- Strong Password generator project.py
+++ Strong Password generator project.py
@@ -22,47 +22,11 @@ for i in range(Special_Symbols):
 
 random.shuffle(Password)
 
-# print(Password)
 Final_Password=" "
 for i in Password:
     Final_Password=Final_Password + i
 print(Final_Password)
 
-
-# .Find and remove a specific element from the list 
-# List=[1,2,3,4,5,6]
-# print(List[2])
-
-# List=[1,2,3,4,5,6]
-# List.remove(4)
-# print(List)
-
-
-# find and update the specific element in the string list
-
-
-# Find max and min in the list using one loop
-
-
-# find average of the list
-
-# Num=(1,2,3,4,5,6,7,8)
-# Average=0
-# for i in Num:
-#     Average=Average + i
-# Average= Average / 8    
-# print(Average)
-
-
-
-
-def Total():
-    Num1=5
-    Num2=10
-    sum=Num1+Num2
-    print(sum)
-
-Total()    
 
 
 
